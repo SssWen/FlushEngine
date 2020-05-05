176a8342eafc8badc2dfb1e4de2ddc034b520145
JerryWen <wen18840839539@gmail.com>
2020-05-05 15:13:09 +0800

    Fixed orthocamera movement.

--- Flush/src/Platform/OpenGL/OpenGLContext.cpp
+++ Flush/src/Platform/OpenGL/OpenGLContext.cpp
@@ -22,7 +22,15 @@ namespace Flush
 		Flush_CORE_INFO(" Vendor: {0} ", glGetString(GL_VENDOR));		
 		Flush_CORE_INFO(" Renderer: {0}", glGetString(GL_RENDERER));		
 		Flush_CORE_INFO(" Version: {0}", glGetString(GL_VERSION));
-		
+
+#ifdef FLUSH_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+		// Check for Opengl Version
+		FLUSH_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "FlushEngine requires at least OpenGL version 4.5!");
+#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
--- Flush/src/Platform/OpenGL/OpenGLShader.cpp
+++ Flush/src/Platform/OpenGL/OpenGLShader.cpp
@@ -192,7 +192,8 @@ namespace Flush {
 
 			size_t nextLinePos = source.find_first_not_of("\r\n", eol);
 			pos = source.find(typeToken, nextLinePos);
-			shaderSources[ShaderTypeFromString(type)] = source.substr(nextLinePos, pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos));
+			//shaderSources[ShaderTypeFromString(type)] = source.substr(nextLinePos, pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos));
+			shaderSources[ShaderTypeFromString(type)] = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
 		}
 
 		return shaderSources;
@@ -263,7 +264,10 @@ namespace Flush {
 		}
 
 		for (auto id : glShaderIDs)
+		{
 			glDetachShader(program, id);
+			glDeleteShader(id);
+		}
 	}
 	void OpenGLShader::Bind() const
 	{
